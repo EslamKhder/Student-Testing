d0bd4cb31d01a647eeea0d525cc4037536939064
eslam khder <eslamkhder11@gmail.com>
2022-12-19 14:27:59 +0200

    impl all method in service

--- src/main/java/com/spring/student/service/StudentServiceImpl.java
+++ src/main/java/com/spring/student/service/StudentServiceImpl.java
@@ -1,36 +1,67 @@
 package com.spring.student.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.spring.student.Repo.StudentRepo;
 import com.spring.student.model.Student;
 import com.spring.student.model.StudentDto;
 
 @Service
 public class StudentServiceImpl implements StudentService{
+
+    @Autowired
+    private StudentRepo studentRepo;
+
     @Override
     public StudentDto createStudent(Student student) {
-        return null;
+        return studentDto(studentRepo.save(student)); // not send id
     }
 
     @Override
     public List<StudentDto> getAllStudent(Student student) {
-        return null;
+        return studentListDto(studentRepo.findAll());
+
     }
 
     @Override
     public void deleteStudent(Long id) {
-
+        studentRepo.deleteById(id);
     }
 
     @Override
     public StudentDto editStudent(Student student) {
-        return null;
+        return studentDto(studentRepo.save(student)); // send id
     }
 
     @Override
     public StudentDto getStudentById(Long id) {
-        return null;
+        return studentDto(studentRepo.findById(id).get());
+    }
+
+    private StudentDto studentDto(Student student){
+        StudentDto studentDto = new StudentDto();
+        studentDto.setId(student.getId());
+        studentDto.setName(student.getName());
+        studentDto.setAge(student.getAge());
+        studentDto.setPhone(student.getPhone());
+        studentDto.setActive(student.isActive());
+        return studentDto;
+    }
+    private List<StudentDto> studentListDto(List<Student> students){
+        List<StudentDto> studentListDto = new ArrayList<>();
+        for (Student student: students){
+            StudentDto studentDto = new StudentDto();
+            studentDto.setId(student.getId());
+            studentDto.setName(student.getName());
+            studentDto.setAge(student.getAge());
+            studentDto.setPhone(student.getPhone());
+            studentDto.setActive(student.isActive());
+            studentListDto.add(studentDto);
+        }
+        return studentListDto;
     }
 }
