f924aef9f90d5f345ba678e0001fee8e4a5210b7
eslam khder <eslamkhder11@gmail.com>
2022-12-22 02:02:02 +0200

    createStudent IT validate data

--- src/test/java/com/spring/student/controller/StudentControllerIT.java
+++ src/test/java/com/spring/student/controller/StudentControllerIT.java
@@ -8,25 +8,36 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.spring.student.StudentApplicationIT;
 import com.spring.student.model.Student;
+import com.spring.student.model.StudentDto;
 import com.spring.student.util.RequestUtil;
 
 public class StudentControllerIT extends StudentApplicationIT {
 
     @Autowired
     private RequestUtil requestUtil;
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
-    public void createStudent_thenValidate(){
+    public void createStudent_thenValidate() throws JsonProcessingException {
         Student student = new Student();
         student.setName("Eslam");
         student.setAge(22);
         student.setPhone("01113903660");
         student.setActive(true);
-        ResponseEntity<Object> responseEntity =
-                requestUtil.post("/api/create",student,null,Object.class);
+        ResponseEntity<String> responseEntity =
+                requestUtil.post("/api/create",student,null,String.class);
         Assertions.assertEquals(HttpStatus.CREATED,responseEntity.getStatusCode());
+
+        StudentDto studentDto = objectMapper.readValue(responseEntity.getBody(),StudentDto.class);
+        Assertions.assertEquals("Eslam",studentDto.getName());
+        Assertions.assertEquals(22,studentDto.getAge());
+        Assertions.assertEquals("01113903660",studentDto.getPhone());
+        Assertions.assertTrue(studentDto.isActive());
     }
 
     @Test
@@ -39,7 +50,7 @@ public class StudentControllerIT extends StudentApplicationIT {
     @Test
     public void deleteStudent_thenValidate(){
         ResponseEntity<Object> responseEntity =
-                requestUtil.delete("/api/delete/1",null,null,Object.class);
+                requestUtil.delete("/api/delete/2",null,null,Object.class);
         Assertions.assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
     }
 
