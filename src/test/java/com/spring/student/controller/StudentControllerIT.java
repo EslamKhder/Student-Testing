0e9833ae71c99a7fa509bcee386939e1101cc635
eslam khder <eslamkhder11@gmail.com>
2022-12-22 16:40:36 +0200

    Clean Code

--- src/test/java/com/spring/student/controller/StudentControllerIT.java
+++ src/test/java/com/spring/student/controller/StudentControllerIT.java
@@ -30,111 +30,103 @@ public class StudentControllerIT extends StudentApplicationIT {
 
     @Test
     public void createStudent_thenValidate() throws JsonProcessingException {
-        Student student = new Student();
-        student.setName("Eslam");
-        student.setAge(22);
-        student.setPhone("01113903660");
-        student.setActive(true);
+        // Given Student
+        Student student = new Student("Eslam",22,"01113903660",true);
+
+        // When Create Student
         ResponseEntity<String> responseEntity =
                 requestUtil.post("/api/create",student,null,String.class);
-        Assertions.assertEquals(HttpStatus.CREATED,responseEntity.getStatusCode());
 
+        // Then Validate
+        Assertions.assertEquals(HttpStatus.CREATED,responseEntity.getStatusCode());
         StudentDto studentDto = objectMapper.readValue(responseEntity.getBody(),StudentDto.class);
         Assertions.assertEquals("Eslam",studentDto.getName());
         Assertions.assertEquals(22,studentDto.getAge());
         Assertions.assertEquals("01113903660",studentDto.getPhone());
         Assertions.assertTrue(studentDto.isActive());
 
-        //studentRepo.deleteById(studentDto.getId());
+        // Clean Data
         dataUtil.delete(studentDto);
     }
 
     @Test
     public void getAllStudent_thenValidate() throws JsonProcessingException {
-        Student student1 = new Student();
-        student1.setName("Eslam");
-        student1.setAge(22);
-        student1.setPhone("01113903660");
-        student1.setActive(true);
+        //Given 2 Student
+        createStudent("Eslam",22,"01113903660",true);
+        createStudent("Ahmed",22,"010258875",false);
 
-        Student student2 = new Student();
-        student2.setName("Ahmed");
-        student2.setAge(22);
-        student2.setPhone("010258875");
-        student2.setActive(false);
-
-        studentRepo.save(student1);
-        studentRepo.save(student2);
-
+        // When Gat All Student
         ResponseEntity<String> responseEntity =
                 requestUtil.get("/api/getAll",null,null,String.class);
-        Assertions.assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
 
+        // Then Validate
+        Assertions.assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
         StudentDto[] studentListDto = objectMapper.readValue(responseEntity.getBody(),StudentDto[].class);
         Assertions.assertEquals(2,studentListDto.length);
-
         Assertions.assertEquals("Ahmed",studentListDto[1].getName());
         Assertions.assertEquals(22,studentListDto[1].getAge());
         Assertions.assertEquals("010258875",studentListDto[1].getPhone());
         Assertions.assertFalse(studentListDto[1].isActive());
 
-        /*studentRepo.deleteById(student1.getId());
-        studentRepo.deleteById(student2.getId());*/
+        // Clean Data
         dataUtil.delete(studentListDto);
     }
 
     @Test
     public void deleteStudent_thenValidate(){
-        Student student = new Student();
-        student.setName("Eslam");
-        student.setAge(22);
-        student.setPhone("01113903660");
-        student.setActive(true);
-        student = studentRepo.save(student);
+        // Given Student
+        Student student = createStudent("Eslam",22,"01113903660",true);
+
+        // When Delete Student
         ResponseEntity<String> responseEntity =
                 requestUtil.delete(String.format("/api/delete/%s",student.getId()),null,null,String.class);
+        // Then Validate
         Assertions.assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
     }
 
     @Test
     public void editStudent_thenValidate() throws JsonProcessingException {
-        Student student = new Student();
-        student.setName("Karim");
-        student.setAge(21);
-        student.setPhone("0122588885");
-        student.setActive(false);
-        student = studentRepo.save(student);
+        // Given Student
+        Student student = createStudent("Karim",21,"0122588885",false);
         student.setName("Ahmed");
+
+        // When Edit Student
         ResponseEntity<String> responseEntity =
                 requestUtil.put("/api/edit",student,null,String.class);
+
+        // Then Validate
         StudentDto studentDto = objectMapper.readValue(responseEntity.getBody(),StudentDto.class);
         Assertions.assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
         Assertions.assertEquals("Ahmed",studentDto.getName());
 
-        //studentRepo.deleteById(student.getId());
+        // Clean Data
         dataUtil.delete(studentDto);
 
     }
 
     @Test
     public void getStudentById_thenValidate() throws JsonProcessingException {
-        Student student = new Student();
-        student.setName("Karim");
-        student.setAge(21);
-        student.setPhone("0122588885");
-        student.setActive(false);
-        student = studentRepo.save(student);
+        // Given Student
+        Student student = createStudent("Karim",21,"0122588885",false);
+
+        // When Get Student By ID
         ResponseEntity<String> responseEntity =
                 requestUtil.get(String.format("/api/get/%s",student.getId()),null,null,String.class);
         StudentDto studentDto = objectMapper.readValue(responseEntity.getBody(),StudentDto.class);
-        Assertions.assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
 
+        // Then Validate
+        Assertions.assertEquals(HttpStatus.OK,responseEntity.getStatusCode());
         Assertions.assertEquals("Karim",studentDto.getName());
         Assertions.assertEquals(21,studentDto.getAge());
         Assertions.assertEquals("0122588885",studentDto.getPhone());
         Assertions.assertFalse(studentDto.isActive());
 
-        //studentRepo.deleteById(student.getId());
+        // Clean Data
         dataUtil.delete(studentDto);
+    }
+
+    private Student createStudent(String name, int age, String phone, boolean active){
+        Student student = new Student(name,age,phone,active);
+        return studentRepo.save(student);
     }
 }
