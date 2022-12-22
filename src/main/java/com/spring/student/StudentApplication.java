209ea892fd8d3aa0e5a6d07fa3bddec4234c7588
eslam khder <eslamkhder11@gmail.com>
2022-12-22 17:07:33 +0200

    Add H2 DataBase

--- src/main/java/com/spring/student/StudentApplication.java
+++ src/main/java/com/spring/student/StudentApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentTestingApplication {
+public class StudentApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StudentTestingApplication.class, args);
+        SpringApplication.run(StudentApplication.class, args);
     }
 
 }
